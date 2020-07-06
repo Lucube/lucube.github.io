a9eac769c17b2abebdfb2a6e63ad82e70de5abaa
Lucube <luki.vogl@gmail.com>
2020-07-06 21:52:55 +0200

    api test in files tft_match_history.html (secondary site) and ajax_request.js

---
+++ ajax_request.js
@@ -0,0 +1,19 @@
+function init() {
+	var endpoint1 = "https://euw1.api.riotgames.com/tft/summoner/v1/summoners/by-name/Tivorak";
+	var xhttp = new XMLHttpRequest();
+	
+	var apikey = document.getElementById("apikey").value;
+
+	xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		document.getElementById("outputtext").innerHTML = this.responseText;
+	}
+	else {
+		document.getElementById("outputtext").innerHTML = this.status;
+	}};
+	alert(window.location.href);
+	xhttp.open("GET", endpoint1, true);
+	xhttp.setRequestHeader("Origin", window.location.href);
+	xhttp.setRequestHeader("X-Riot-Token", apikey);
+	xhttp.send();
+}
