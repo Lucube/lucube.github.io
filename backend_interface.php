bef2a2b625e129a6128423a62562392e3ddef215
Lucube <luki.vogl@gmail.com>
2020-10-04 21:36:15 +0200

    v1.0 TFT match history, endpoint needs to be changed

---
+++ backend_interface.php
@@ -0,0 +1,30 @@
+<?php
+  // Request Handling
+  header('Access-Control-Allow-Origin: http://localhost:63342');
+
+  // Pass url parameter as url
+  $url = $_POST["url"];
+  $query = array(
+      'api_key' => $_POST["token"]
+  );
+  // additional parameters
+  //$query['count'] = '20';
+
+  // Response from RIOT API
+  $options = array(
+      'http' => array(
+          'header' => "Accept-Charset: application/x-www-form-urlencoded\r\n" .
+              "X-Riot-Token: " . $_POST["token"],
+          'method' => 'GET',
+          'content' => $query
+      )
+  );
+
+  $context = stream_context_create($options);
+  $result = file_get_contents($url, false, $context);
+
+  if ($result === FALSE) {
+      echo 'Error in request';
+  }
+
+  echo $result;
